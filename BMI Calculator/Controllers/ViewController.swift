428e2888613d0e9039a11cbb177a0085abd21835
midhun-v <narenmids@gmail.com>
2021-05-15 10:25:56 +0530

    Applied Segue - Multi view controllers

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -10,11 +10,42 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var heightValue: UILabel!
+    @IBOutlet weak var weightValue: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    var bmi: Float = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func sliderMoved(_ sender: UISlider) {
+        if sender.tag == 1{
+            heightValue.text = String(format: "%.2fm",sender.value)
+        }
+        else{
+            weightValue.text = String(format: "%.0fkg",sender.value)
+        }
+    }
+    
+    @IBAction func calculate(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        bmi = (weight/pow(height,2))
+        print(bmi)
+        
+        self.performSegue(withIdentifier: "resultSegue", sender: self)
 
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "resultSegue"{
+            let destinationVC = segue.destination as! ResultViewController
+            destinationVC.bmiValue = String(format: "%.2f", bmi)
+        }
+    }
 }
 
